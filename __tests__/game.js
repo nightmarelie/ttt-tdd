ff90351b5872857049d94ea19bb06796489cf796
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-03-10 23:27:01 -0600

    refactor: optimize test

--- __tests__/game.js
+++ __tests__/game.js
@@ -1,15 +1,22 @@
 import { Game } from "../src/game";
 
+const initialGameBoardState = [
+  ["", "", ""],
+  ["", "", ""],
+  ["", "", ""],
+];
+
 describe("Game", () => {
+  let game;
+  beforeEach(() => {
+    game = new Game();
+  });
+
   it("should return empty game board", () => {
     const game = new Game();
 
     const board = game.getState();
 
-    expect(board).toEqual([
-      ["", "", ""],
-      ["", "", ""],
-      ["", "", ""],
-    ]);
+    expect(board).toEqual(initialGameBoardState);
   });
 });
