12ffda391a23cfd19352837344100d58bb0b3632
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-03-13 19:28:05 -0600

    refactor: game class

--- src/game.js
+++ src/game.js
@@ -5,6 +5,7 @@ class Game {
     this._userName = "user";
     this._computerName = "computer";
     this._history = [];
+    this._fieldSize = 3;
     this._board = [
       ["", "", ""],
       ["", "", ""],
@@ -23,8 +24,8 @@ class Game {
   }
 
   createComputerMove() {
-    const x = Math.floor(Math.random() * (3 - 0));
-    const y = Math.floor(Math.random() * (3 - 0));
+    const x = this._getRandomCoordinate();
+    const y = this._getRandomCoordinate();
 
     this._updateHistory(this._computerName, x, y);
     this._updateBoard(x, y, {
@@ -54,6 +55,10 @@ class Game {
 
   _updateHistory(turn, x, y) {
     this._history.push({ turn, x, y });
+  }
+
+  _getRandomCoordinate() {
+    return Math.floor(Math.random() * (this._fieldSize - 0));
   }
 }
 
