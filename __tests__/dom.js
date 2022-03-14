d04b1fd577ea4e4ae1ec7a9393cb49ade7baa44b
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-03-13 20:48:46 -0600

    test: check creates table with rows and columns

--- __tests__/dom.js
+++ __tests__/dom.js
@@ -13,11 +13,25 @@ global.window = dom.window;
 global.document = dom.window.document;
 
 describe("DOM controller", () => {
+  afterEach(() => {
+    document.body.innerHTML = "";
+  });
+
   test("creates empty table", () => {
     const domController = createInstance();
 
     domController.createTable();
 
     expect(document.querySelectorAll("table").length).toBe(1);
+  });
+
+  test("Creates table with 3 rows and 3 columns", () => {
+    const domController = createInstance();
+
+    domController.createTable(3, 3);
+
+    expect(document.querySelectorAll("table").length).toBe(1);
+    expect(document.querySelectorAll("tr").length).toBe(3);
+    expect(document.querySelectorAll("td").length).toBe(9);
   });
 });
