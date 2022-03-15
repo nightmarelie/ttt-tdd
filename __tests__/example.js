6de830c3697fb01acf802f54ee5b8d23daaff001
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-03-14 22:35:59 -0600

    test: simple mathers toEqual for object

--- __tests__/example.js
+++ __tests__/example.js
@@ -2,4 +2,14 @@ describe("Using mathers", () => {
   it("two plus two is four", () => {
     expect(2 + 2).toBe(4);
   });
+
+  it("object assigment", () => {
+    const data = { one: 1 };
+    data["two"] = 2;
+
+    expect(data).toEqual({
+      one: 1,
+      two: 2,
+    });
+  });
 });
