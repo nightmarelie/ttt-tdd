ec21cc59ced56286f761676ead99fb4277d354aa
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-03-14 22:43:58 -0600

    test: simple floating point number matcher

--- __tests__/example.js
+++ __tests__/example.js
@@ -41,4 +41,10 @@ describe("Using mathers", () => {
     expect(value).toBe(4);
     expect(value).toEqual(4);
   });
+
+  it("adding floating point numbers", () => {
+    const value = 0.1 + 0.2;
+
+    expect(value).toBeCloseTo(0.3);
+  });
 });
