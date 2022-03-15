e7d30c9e36b85756b7d6fae19acdc7c428fd4543
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-03-14 22:36:53 -0600

    test: simple truthiness

--- __tests__/example.js
+++ __tests__/example.js
@@ -12,4 +12,22 @@ describe("Using mathers", () => {
       two: 2,
     });
   });
+
+  it("null", () => {
+    const n = null;
+    expect(n).toBeNull();
+    expect(n).toBeDefined();
+    expect(n).not.toBeUndefined();
+    expect(n).not.toBeTruthy();
+    expect(n).toBeFalsy();
+  });
+
+  it("zero", () => {
+    const z = 0;
+    expect(z).not.toBeNull();
+    expect(z).toBeDefined();
+    expect(z).not.toBeUndefined();
+    expect(z).not.toBeTruthy();
+    expect(z).toBeFalsy();
+  });
 });
