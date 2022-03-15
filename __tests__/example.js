bdc4780259b43f5c1a0d79f29ffb79327b17918b
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-03-14 22:41:14 -0600

    test: simple numbers matchers

--- __tests__/example.js
+++ __tests__/example.js
@@ -30,4 +30,15 @@ describe("Using mathers", () => {
     expect(z).not.toBeTruthy();
     expect(z).toBeFalsy();
   });
+
+  it("two plus two", () => {
+    const value = 2 + 2;
+    expect(value).toBeGreaterThan(3);
+    expect(value).toBeGreaterThanOrEqual(3.5);
+    expect(value).toBeLessThan(5);
+    expect(value).toBeLessThanOrEqual(5);
+
+    expect(value).toBe(4);
+    expect(value).toEqual(4);
+  });
 });
